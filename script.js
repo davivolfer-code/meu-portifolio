e0aec2662aba6702d2006ba5c7675ff811669506
davivolfer-code <davivolfer@gmail.com>
2025-11-25 12:06:16 -0300

    commit alterações básicas e adicionando meu novo projeto

---
+++ script.js
@@ -0,0 +1,10 @@
+// Animação suave na rolagem
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      target.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
